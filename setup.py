2bbf33da141c6c9d07602ad8d417a200ede62425
José Ramón Hernández Aguilar <78838898+JRamonHA@users.noreply.github.com>
2025-02-24 01:16:07 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='datatools',
-    version='0.1',
+    version='0.1'
+    author=['José Ramón Hernández'],
+    author_email=['jrha@ier.unam.mx'],
+    url='https://github.com/JRamonHA/datatools',
+    license='MIT',
+    packages=['datatools'],
     packages=find_packages(),
     install_requires=['pandas'],
 )
