ab6253d761f69bfd8d69e9f05595c202784d38e5
José Ramón Hernández Aguilar <78838898+JRamonHA@users.noreply.github.com>
2025-02-24 10:53:31 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='datatools',
-    version='0.1'
+    version='0.0.1'
     author=['José Ramón Hernández'],
     author_email=['jrha@ier.unam.mx'],
     url='https://github.com/JRamonHA/datatools',
